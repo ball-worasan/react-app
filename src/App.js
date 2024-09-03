243e01bae8fe9bd91d0bfda416bd499a4ed10e36
ball-worasan <thanaporn01as@gmail.com>
2024-09-03 17:06:28 +0700

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -35,9 +35,6 @@ const App = () => {
                             Dashboard
                         </button>
                     </Link>
-                    <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-                        Logout
-                    </button>
                 </div>
             </header>
 
